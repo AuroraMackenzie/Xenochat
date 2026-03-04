f7694981c0d35c4f389486b23609b38bc55b8e82
AuroraMackenzie <zishuchengchengmxy@gmail.com>
2026-03-04 18:14:38 +0800

    chore: exclude local blueprint and summary files from mirror

--- scripts/sync_to_github_mirror.sh
+++ scripts/sync_to_github_mirror.sh
@@ -11,7 +11,11 @@ rsync -a --delete \
   --exclude '.Xenochat' \
   --exclude 'node_modules' \
   --exclude 'frontend/dist' \
+  --exclude 'Xenochat项目蓝图.txt' \
+  --exclude '前身项目总结.txt' \
   --exclude '.DS_Store' \
   "$SOURCE_DIR/" "$MIRROR_DIR/"
+
+rm -f "$MIRROR_DIR/Xenochat项目蓝图.txt" "$MIRROR_DIR/前身项目总结.txt"
 
 echo "Synced Xenochat to mirror: $MIRROR_DIR"
